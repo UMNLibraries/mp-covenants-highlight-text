29fc578bd98ec5944db567d09f0a9b1dc40ae7a3
Michael Corey <corey101@umn.edu>
2024-10-28 15:04:19 -0500

    Mostly working with regular and fuzzy search, including handwriting

--- highlight_text/app.py
+++ highlight_text/app.py
@@ -1,4 +1,6 @@
 import io
+import re
+import regex
 import sys
 import json
 import math
@@ -20,48 +22,112 @@ def load_json(bucket, key):
     return json.loads(file_content)
 
 
-def isolate_term_hit(term, line_words):
+def test_term_search(next_part, haystack, fuzzy=False):
+    '''used on both lines and word objects. If fuzzy, use regex (no re) fuzzy module. If not, use basic CNTL + F search'''
+    # print(fuzzy)
+    # test_word = haystack_word['Text'].lower()
+    if fuzzy:
+        tolerance = "{e<=3}"
+        next_part_fuzzy = regex.compile(f"(?:\\b{next_part}){tolerance}")
+        print(next_part_fuzzy, haystack)
+        if regex.search(next_part_fuzzy, haystack):
+            return True
+    else:
+        if next_part in haystack:
+            return True
+    return False
+
+
+def isolate_term_hit(term, line_words, fuzzy=False):
     # Make sure we are latching on to the full term match, not an incidental word that makes it up
+    # This will need some work for fuzzy search
     term_split = term.strip().split(' ')
     term_parts_found = []
     words_to_highlight = []
 
+    print(term, line_words, fuzzy)
+
     for word in line_words:
-        # print(f"Word: \"{word['Text'].lower()}\"")
+        print(f"Word: \"{word['Text'].lower()}\"")
         # This next line is meant to be "If this word is the next one you're looking for"
-        if word['Text'].lower() == term_split[len(term_parts_found)]:
-            # print(f"Found {word['Text'].lower()}")
-            term_parts_found.append(word['Text'].lower())
+        # if re.match(f"({term_split[len(term_parts_found)]})", word['Text'].lower()):
+
+        # TODO: edit for fuzzy?
+        next_part = term_split[len(term_parts_found)]
+
+        if test_term_search(next_part, word['Text'].lower(), fuzzy):
+        # tolerance = "{e<=3}"
+        # next_part_fuzzy = regex.compile(f"(?:{next_part}){tolerance}")
+        # if regex.search(next_part_fuzzy, word['Text'].lower()):
+        # # if next_part in word['Text'].lower():
+            print(f"Found {word['Text'].lower()}")
+            term_parts_found.append(term_split[len(term_parts_found)])
             words_to_highlight.append(word)
+            print(len(term_parts_found), len(term_split))
             if len(term_parts_found) < len(term_split):
                 # Go to next word
                 pass
             elif len(term_parts_found) >= len(term_split):
+                print(term_parts_found, term_split)
                 if term_parts_found == term_split:
                     return words_to_highlight
                 else:
                     # Nope, wrong next word, reset and move to next word
                     term_parts_found = []
                     words_to_highlight = []
-    return False
+    return words_to_highlight
+
+def make_highlight_box(overlay, img_width, img_height, word_obj):
+
+    word_box = word_obj['Geometry']['BoundingBox']
+    print(word_box)
+
+    draw = ImageDraw.Draw(overlay)
+
+    x1 = word_box['Left'] * img_width
+    y1 = word_box['Top'] * img_height
+    x2 = x1 + (word_box['Width'] * img_width)
+    y2 = y1 + (word_box['Height'] * img_height)
+
+    draw.rectangle(((x1, y1), (x2, y2)), fill=(255, 255, 0, 100))
+
+    return overlay
 
 def add_highlights(bucket, in_path, out_path, word_objs):
+    # print(bucket, in_path, word_objs)
     response = s3.get_object(Bucket=bucket, Key=in_path)
-    print("CONTENT TYPE: " + response['ContentType'])
     try:
-        im = Image.open(in_tiff_body)
+        im = Image.open(response['Body']).convert('RGBA')
     except Image.UnidentifiedImageError as err:
         raise
         return None
+    
+    width, height = im.size
 
+    overlay = Image.new('RGBA', im.size)
 
-        # return response['ContentType']
-        #
-        # # print(key, key.replace('.tif', '.jpg'))
-        #
-        # out_jpg_buffer = save_jpeg_to_target_size(
-        #     key, response['Body'], 1000000, True, True)
-    return False
+    for word_obj in word_objs:
+        overlay = make_highlight_box(overlay, width, height, word_obj)
+
+    im = Image.alpha_composite(im, overlay)
+    im = im.convert("RGB") # Remove alpha for saving in jpg format.
+
+    out_highlight_buffer = io.BytesIO()
+    im.save(out_highlight_buffer, format="JPEG")
+    out_highlight_buffer.seek(0)
+
+    # Upload resized image to destination bucket
+    s3.put_object(
+        Body=out_highlight_buffer,
+        Bucket=bucket,
+        Key=out_path,
+        StorageClass='GLACIER_IR',
+        ContentType='image/jpeg',
+        ACL='public-read'
+    )
+
+    return out_path
+
 
 def lambda_handler(event, context):
     '''
@@ -78,6 +144,8 @@ def lambda_handler(event, context):
         bucket = event['Records'][0]['s3']['bucket']['name']
         key = urllib.parse.unquote_plus(
             event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+        orig_img = urllib.parse.unquote_plus(
+            event['Records'][0]['s3']['object']['orig'], encoding='utf-8')
         public_uuid = urllib.parse.unquote_plus(
             event['Records'][0]['s3']['object']['uuid'], encoding='utf-8')
         bool_hit = True
@@ -85,11 +153,13 @@ def lambda_handler(event, context):
     #     # Get object from step function with this as first step
     #     bucket = event['detail']['bucket']['name']
     #     key = event['detail']['object']['key']
+    #     orig_img = event['detail']['object']['orig']
     #     public_uuid = None
     else:
         # Most common: Coming from previous step function lambda, which should be output from mp-term-search-basic
         bucket = event['body']['bucket']
-        key = event['body']['json']
+        key = event['body']['match_file']
+        orig_img = event['body']['orig']
         public_uuid = event['body']['uuid']
         bool_hit = event['body']['bool_hit']
 
@@ -103,7 +173,8 @@ def lambda_handler(event, context):
 
     # Get OCR json data
     try:
-        ocr_json_key = key.replace('ocr/hits', 'ocr/json')
+        # ocr_json_key = key.replace('ocr/hits', 'ocr/json')
+        ocr_json_key = re.sub(r'ocr/hits(?:_fuzzy)?', 'ocr/json', key)
         ocr_result = load_json(bucket, ocr_json_key)
         lines = [block for block in ocr_result['Blocks'] if block['BlockType'] == 'LINE']
         words  = [block for block in ocr_result['Blocks'] if block['BlockType'] == 'WORD']
@@ -113,17 +184,26 @@ def lambda_handler(event, context):
         raise e
 
     print(term_search_result)
-    matched_terms = [key for key in term_search_result.keys() if key not in ['workflow', 'lookup', 'uuid']]
-    print(matched_terms)
+    matched_term_objs = [{'term': term_key, 'line_nums': line_nums} for term_key, line_nums in term_search_result.items() if term_key not in ['workflow', 'lookup', 'uuid']]
+    print(matched_term_objs)
 
     # print(words)
     print("Scanning for term objects on page to highlight...")
     words_to_highlight = []
 
+    # Check if this is a fuzzy result or traditional CNTRL + F
+    if 'hits_fuzzy' in key:
+        print('this is fuzzy')
+        fuzzy = True
+    else:
+        fuzzy = False
+
     for line_num, line in enumerate(lines):
-        for term in matched_terms:
-            if term in line['Text'].lower():
-                # print(f'\nFound term {term} in line {line_num}\n')
+        for term_obj in matched_term_objs:
+            if line_num in term_obj['line_nums']:
+            # if test_term_search(term, line['Text'].lower(), fuzzy):
+            # if term in line['Text'].lower():
+                print(f"\nChecking for term {term_obj['term']} in line {line_num}\n")
                 # print(line)
                 word_children = [objs['Ids'] for objs in line['Relationships'] if objs['Type'] == 'CHILD']
                 if len(word_children) > 0:
@@ -131,26 +211,32 @@ def lambda_handler(event, context):
                     line_words = [word for word in words if word['Id'] in word_children[0]]
 
                     # Make sure we are latching on to the full term match, not an incidental word that makes it up
-                    words_to_highlight += isolate_term_hit(term, line_words)
+                    words_to_highlight += isolate_term_hit(term_obj['term'], line_words, fuzzy)
 
     print(words_to_highlight)
 
     if len(words_to_highlight) > 0:
         print(f"Found {len(words_to_highlight)} words to highlight.")
 
-        # img_in_path = key.replace('ocr/hits', 'web').replace('.json', f"{uuid}.jpg")
-        img_in_path = str(PurePath(key.replace('ocr/hits', 'web')).with_name(public_uuid + '.jpg'))
-        img_out_path = str(PurePath(key.replace('ocr/hits', 'web')).with_name(public_uuid + '__highlight.jpg'))
-        highlight_key = add_highlights(bucket, in_path, out_path, word_objs)
+        img_in_path = str(PurePath(re.sub(r'ocr/hits(?:_fuzzy)?', 'web', key)).with_name(public_uuid + '.jpg'))
+        img_out_path = str(PurePath(re.sub(r'ocr/hits(?:_fuzzy)?', 'web_highlighted', key)).with_name(public_uuid + '__highlight.jpg'))
+        highlight_key = add_highlights(bucket, img_in_path, img_out_path, words_to_highlight)
+
+        status = 200
+        message = "highlight test success"
+
+    else:
+        status = 400
+        message = 'Error: No highlightable text found'
+        highlight_key = None
 
     return {
-        "statusCode": 200,
+        "statusCode": status,
         "body": {
-            "message": "hello world",
+            "message": message,
             "bucket": bucket,
-            "orig_img": key,
+            "orig_img": orig_img,
             "highlighted_img": highlight_key,
             "uuid": public_uuid
-            # "location": ip.text.replace("\n", "")
         }
     }
--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -1,73 +1,259 @@
+import io
 import json
-
+import toml
+import boto3
 import pytest
+from PIL import Image
 
-from hello_world import app
+from highlight_text import app
+
+with open('samconfig.toml', 'r') as f:
+    config = toml.load(f)
+    s3_bucket = config['default']['deploy']['parameters']['s3_bucket']
+    s3_region = config['default']['deploy']['parameters']['region']
+
+s3 = boto3.client('s3')
 
 
-@pytest.fixture()
-def apigw_event():
-    """ Generates API GW Event"""
+def open_s3_image(bucket, key):
+    response = s3.get_object(Bucket=bucket, Key=key)
+    im = Image.open(response['Body'])
+    return im
+
+
+def build_lambda_input(bucket, infile_json, orig, uuid):
 
     return {
-        "body": '{ "test": "body"}',
-        "resource": "/{proxy+}",
-        "requestContext": {
-            "resourceId": "123456",
-            "apiId": "1234567890",
-            "resourcePath": "/{proxy+}",
-            "httpMethod": "POST",
-            "requestId": "c6af9ac6-7b61-11e6-9a41-93e8deadbeef",
-            "accountId": "123456789012",
-            "identity": {
-                "apiKey": "",
-                "userArn": "",
-                "cognitoAuthenticationType": "",
-                "caller": "",
-                "userAgent": "Custom User Agent String",
-                "user": "",
-                "cognitoIdentityPoolId": "",
-                "cognitoIdentityId": "",
-                "cognitoAuthenticationProvider": "",
-                "sourceIp": "127.0.0.1",
-                "accountId": "",
-            },
-            "stage": "prod",
-        },
-        "queryStringParameters": {"foo": "bar"},
-        "headers": {
-            "Via": "1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)",
-            "Accept-Language": "en-US,en;q=0.8",
-            "CloudFront-Is-Desktop-Viewer": "true",
-            "CloudFront-Is-SmartTV-Viewer": "false",
-            "CloudFront-Is-Mobile-Viewer": "false",
-            "X-Forwarded-For": "127.0.0.1, 127.0.0.2",
-            "CloudFront-Viewer-Country": "US",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            "Upgrade-Insecure-Requests": "1",
-            "X-Forwarded-Port": "443",
-            "Host": "1234567890.execute-api.us-east-1.amazonaws.com",
-            "X-Forwarded-Proto": "https",
-            "X-Amz-Cf-Id": "aaaaaaaaaae3VYQb9jd-nvCd-de396Uhbp027Y2JvkCPNLmGJHqlaA==",
-            "CloudFront-Is-Tablet-Viewer": "false",
-            "Cache-Control": "max-age=0",
-            "User-Agent": "Custom User Agent String",
-            "CloudFront-Forwarded-Proto": "https",
-            "Accept-Encoding": "gzip, deflate, sdch",
-        },
-        "pathParameters": {"proxy": "/examplepath"},
-        "httpMethod": "POST",
-        "stageVariables": {"baz": "qux"},
-        "path": "/examplepath",
+        "statusCode": 200,
+        "body": {
+            "message": "hit test",
+            "bool_hit": True,
+            "bucket": bucket,
+            "match_file": infile_json,
+            "uuid": uuid,
+            "orig": orig
+        }
     }
 
 
-def test_lambda_handler(apigw_event, mocker):
+@pytest.fixture()
+def mn_wash_hit_death_cert():
+    """ Generates API GW Event"""
+    return build_lambda_input(
+        s3_bucket,
+        "ocr/hits/mn-washington-county/1900 A DEEDS B121-160/1900/1900_01_01_A_NONE_DEED_121_111_2478363_SPLITPAGE_1.json",
+        "raw/mn-washington-county/1900 A DEEDS B121-160/1900/1900_01_01_A_NONE_DEED_121_111_2478363_SPLITPAGE_1.tif",
+        "e7a996f2d7cc484abeaf51eaa07dd096"
+    )
 
-    ret = app.lambda_handler(apigw_event, "")
-    data = json.loads(ret["body"])
+
+@pytest.fixture()
+def mn_anoka_hit_negro_splitpage():
+    """ Generates API GW Event"""
+    return build_lambda_input(
+        s3_bucket,
+        "ocr/hits/mn-anoka-county/1/26362914_SPLITPAGE_1.json",
+        "raw/mn-anoka-county/1/26362914_SPLITPAGE_1.tif",
+        "0e2b9d2dea9d414e97d9c9a5ef8c70a3"
+    )
+
+@pytest.fixture()
+def mn_anoka_hit_multi_term_splitpage():
+    """ Generates API GW Event"""
+    return build_lambda_input(
+        s3_bucket,
+        "ocr/hits/mn-anoka-county/9/30680019.json",
+        "raw/mn-anoka-county/9/30680019.tif",
+        "280ebcf644c94506a65248ce70ddf9bf"
+    )
+
+@pytest.fixture()
+def wi_milw_hit_handwritten():
+    """ Generates API GW Event"""
+    return build_lambda_input(
+        s3_bucket,
+        "ocr/hits/wi-milwaukee-county/30000101/00997952_NOTINDEX_0001.json",
+        "raw/wi-milwaukee-county/30000101/00997952_NOTINDEX_0001.tif",
+        "f2b69676d7e24ae6ae451139cf913c44"
+    )
+
+@pytest.fixture()
+def wi_milw_hit_handwritten_2():
+    """ Generates API GW Event"""
+    return build_lambda_input(
+        s3_bucket,
+        "ocr/hits/wi-milwaukee-county/30000101/01004708_NOTINDEX_0001.json",
+        "raw/wi-milwaukee-county/30000101/01004708_NOTINDEX_0001.tif",
+        "bc56325e45934755973e50a92ca9929c"
+    )
+
+
+@pytest.fixture()
+def wi_milw_hit_fuzzy_but_exact_1():
+    """ Generates API GW Event"""
+    return build_lambda_input(
+        s3_bucket,
+        "ocr/hits_fuzzy/wi-milwaukee-county/17760704/DV0560_DEED_0375.json",
+        "raw/wi-milwaukee-county/17760704/DV0560_DEED_0375.tif",
+        "ffba76108095409e900be6682d78cc1b"
+    )
+
+
+@pytest.fixture()
+def mn_ramsey_hit_fuzzy_but_exact_2():
+    """ Generates API GW Event"""
+    return build_lambda_input(
+        s3_bucket,
+        "ocr/hits_fuzzy/mn-ramsey-county/Batch8/8424793_SPLITPAGE_2.json",
+        "raw/mn-ramsey-county/Batch8/8424793_SPLITPAGE_2.tif",
+        "adc149cffe2a459f9988b7a406a04fc7"
+    )
+
+
+@pytest.fixture()
+def wi_milw_hit_fuzzy_true_fuzzy_1():
+    """ Generates API GW Event"""
+    return build_lambda_input(
+        s3_bucket,
+        "ocr/hits_fuzzy/wi-milwaukee-county/30000102/01847518_NOTINDEX_0001.json",
+        "raw/wi-milwaukee-county/30000102/01847518_NOTINDEX_0001.tif",
+        "446fc261af534bcd9f46bfff00b5d56e"
+    )
+
+
+@pytest.fixture()
+def mn_ramsey_hit_fuzzy_true_fuzzy_2():
+    """ Generates API GW Event"""
+    return build_lambda_input(
+        s3_bucket,
+        "ocr/hits_fuzzy/mn-ramsey-county/Batch25/19048869.json",
+        "raw/mn-ramsey-county/Batch25/19048869.tif",
+        "98484315261a4d8fa6177ebbb278c17e"
+    )
+
+
+
+def test_match_death_cert(mn_wash_hit_death_cert):
+
+    ret = app.lambda_handler(mn_wash_hit_death_cert, "")
+    data = ret["body"]
+    print(data)
 
     assert ret["statusCode"] == 200
-    assert "message" in ret["body"]
-    assert data["message"] == "hello world"
-    # assert "location" in data.dict_keys()
+    assert data["message"] == "highlight test success"
+    
+    # Check if image in correct mode
+    im = open_s3_image(data['bucket'], data['highlighted_img'])
+    assert im.mode == 'RGB'
+
+
+def test_match_negro_splitpage(mn_anoka_hit_negro_splitpage):
+
+    ret = app.lambda_handler(mn_anoka_hit_negro_splitpage, "")
+    data = ret["body"]
+    print(data)
+
+    assert ret["statusCode"] == 200
+    assert data["message"] == "highlight test success"
+
+    # Check if image in correct mode
+    im = open_s3_image(data['bucket'], data['highlighted_img'])
+    assert im.mode == 'RGB'
+
+
+def test_match_multi_term_split(mn_anoka_hit_multi_term_splitpage):
+
+    ret = app.lambda_handler(mn_anoka_hit_multi_term_splitpage, "")
+    data = ret["body"]
+    print(data)
+
+    assert ret["statusCode"] == 200
+    assert data["message"] == "highlight test success"
+
+    # Check if image in correct mode
+    im = open_s3_image(data['bucket'], data['highlighted_img'])
+    assert im.mode == 'RGB'
+
+
+def test_match_handwritten(wi_milw_hit_handwritten):
+
+    ret = app.lambda_handler(wi_milw_hit_handwritten, "")
+    data = ret["body"]
+    print(data)
+
+    assert ret["statusCode"] == 200
+    assert data["message"] == "highlight test success"
+
+    # Check if image in correct mode
+    im = open_s3_image(data['bucket'], data['highlighted_img'])
+    assert im.mode == 'RGB'
+
+
+def test_match_handwritten_2(wi_milw_hit_handwritten_2):
+
+    ret = app.lambda_handler(wi_milw_hit_handwritten_2, "")
+    data = ret["body"]
+    print(data)
+
+    assert ret["statusCode"] == 200
+    assert data["message"] == "highlight test success"
+
+    # Check if image in correct mode
+    im = open_s3_image(data['bucket'], data['highlighted_img'])
+    assert im.mode == 'RGB'
+
+
+def test_match_fuzzy_but_exact_1(wi_milw_hit_fuzzy_but_exact_1):
+
+    ret = app.lambda_handler(wi_milw_hit_fuzzy_but_exact_1, "")
+    data = ret["body"]
+    print(data)
+
+    assert ret["statusCode"] == 200
+    assert data["message"] == "highlight test success"
+
+    # Check if image in correct mode
+    im = open_s3_image(data['bucket'], data['highlighted_img'])
+    assert im.mode == 'RGB'
+
+
+def test_match_fuzzy_but_exact_2(mn_ramsey_hit_fuzzy_but_exact_2):
+
+    ret = app.lambda_handler(mn_ramsey_hit_fuzzy_but_exact_2, "")
+    data = ret["body"]
+    print(data)
+
+    assert ret["statusCode"] == 200
+    assert data["message"] == "highlight test success"
+
+    # Check if image in correct mode
+    im = open_s3_image(data['bucket'], data['highlighted_img'])
+    assert im.mode == 'RGB'
+
+
+def test_match_true_fuzzy_1(wi_milw_hit_fuzzy_true_fuzzy_1):
+
+    ret = app.lambda_handler(wi_milw_hit_fuzzy_true_fuzzy_1, "")
+    data = ret["body"]
+    print(data)
+
+    assert ret["statusCode"] == 200
+    assert data["message"] == "highlight test success"
+
+    # Check if image in correct mode
+    im = open_s3_image(data['bucket'], data['highlighted_img'])
+    assert im.mode == 'RGB'
+
+
+def test_match_true_fuzzy_2(mn_ramsey_hit_fuzzy_true_fuzzy_2):
+
+    ret = app.lambda_handler(mn_ramsey_hit_fuzzy_true_fuzzy_2, "")
+    data = ret["body"]
+    print(data)
+
+    assert ret["statusCode"] == 200
+    assert data["message"] == "highlight test success"
+
+    # Check if image in correct mode
+    im = open_s3_image(data['bucket'], data['highlighted_img'])
+    assert im.mode == 'RGB'
